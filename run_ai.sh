8cb9327cd6bfb123f6c56da1e39785cb37f2d44c
ShellCrafter <muhammedrishdin066@gmail.com>
2026-07-19 19:22:29 +0530

    Update run_ai.sh for improved user interface

--- run_ai.sh
+++ run_ai.sh
@@ -2,26 +2,45 @@
 
 clear
 
-echo "=============================================================="
-echo "   ███████╗██╗  ██╗███████╗██╗     ██╗      ██████╗██████╗"
-echo "   ██╔════╝██║  ██║██╔════╝██║     ██║     ██╔════╝██╔══██╗"
-echo "   ███████╗███████║█████╗  ██║     ██║     ██║     ██████╔╝"
-echo "   ╚════██║██╔══██║██╔══╝  ██║     ██║     ██║     ██╔══██╗"
-echo "   ███████║██║  ██║███████╗███████╗███████╗╚██████╗██║  ██║"
-echo "   ╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝ ╚═════╝╚═╝  ╚═╝"
+echo "=========================================="
+echo "           ShellCrafter AI"
+echo "=========================================="
+echo "Model  : Qwen 3.5 0.8B Q4_K_M"
+echo "Engine : llama.cpp"
 echo
-echo "                 ShellCrafter AI Launcher"
-echo "=============================================================="
+
+echo "Select Mode:"
 echo
-echo " Model : Qwen 3.5 0.8B Q4_K_M"
-echo " Engine: llama.cpp"
-echo " Mode  : No Thinking"
+echo "  1) Instant ⚡ (No Thinking)"
+echo "  2) Thinking 🧠"
 echo
+
+read -p "Choice [1-2]: " MODE
 
 mkdir -p "$HOME/models"
-
 cd "$HOME/models" || exit 1
 
-llama-cli \
--hf unsloth/Qwen3.5-0.8B-GGUF:Q4_K_M \
---chat-template-kwargs '{"enable_thinking":false}'
+case "$MODE" in
+    1)
+        echo
+        echo "Launching Instant Mode..."
+        sleep 1
+        clear
+        llama-cli \
+        -hf unsloth/Qwen3.5-0.8B-GGUF:Q4_K_M \
+        --chat-template-kwargs '{"enable_thinking":false}'
+        ;;
+    2)
+        echo
+        echo "Launching Thinking Mode..."
+        sleep 1
+        clear
+        llama-cli \
+        -hf unsloth/Qwen3.5-0.8B-GGUF:Q4_K_M
+        ;;
+    *)
+        echo
+        echo "Invalid choice."
+        exit 1
+        ;;
+esac
